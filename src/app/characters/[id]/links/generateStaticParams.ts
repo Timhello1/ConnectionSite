c595c8bc783615eff5bd39a998786bb42b88087f
Tymoteusz Całka <calkatym@gmail.com>
2025-12-05 23:09:59 +0100

    fix build

--- src/app/characters/[id]/links/generateStaticParams.ts
+++
@@ -1,7 +0,0 @@
-import { mockCharacters } from '../../../../data/mockCharacters';
-
-export function generateStaticParams() {
-  return mockCharacters.map(character => ({
-    id: character.id,
-  }));
-}
